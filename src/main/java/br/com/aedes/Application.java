b7c4d71637e50a61d674a7df92ac546f60805195
GSuaki <gsuaki@hotmail.com>
2016-01-06 23:19:09 -0200

    Properties config on SpringBootBotstrap

--- src/main/java/br/com/aedes/Application.java
+++ src/main/java/br/com/aedes/Application.java
@@ -1,12 +1,14 @@
 package br.com.aedes;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@SpringBootApplication
-public class Application{
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+@Order(Ordered.LOWEST_PRECEDENCE)
+public class Application extends WebMvcConfigurerAdapter {
+
+	public static void main(final String[] args) throws Exception {
+		new SpringApplicationBuilder(Application.class).run(args);
 	}
-	
 }
---
+++ src/main/java/br/com/aedes/ApplicationServlet.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package br.com.aedes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
+/**
+ * @author GSuaki
+ *
+ */
+@PropertySources({
+    @PropertySource("application.properties"),
+    @PropertySource(value = "${AEDES_CONFIG_PATH}", ignoreResourceNotFound = true)
+})
+@SpringBootApplication
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class ApplicationServlet extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(ApplicationServlet.class);
+  }
+
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(ApplicationServlet.class, args);
+  }
+}
