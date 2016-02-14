039d87d382e590a2f3e79a8e6ad09f21eba42913
GSuaki <gsuaki@hotmail.com>
2016-02-14 19:44:50 -0200

    war

--- src/main/java/br/com/aedes/Application.java
+++ src/main/java/br/com/aedes/Application.java
@@ -1,28 +1,31 @@
 package br.com.aedes;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * @author GSuaki
  *
  */
-//@PropertySources({
-//    @PropertySource(value = "${SPRING_CONFIG_PATH}", ignoreResourceNotFound = true),
-//    @PropertySource("classpath:application.properties")
-//})
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableConfigurationProperties
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class Application extends WebMvcConfigurerAdapter {
+public class Application extends SpringBootServletInitializer {
 
-	public static void main(final String[] args) throws Exception {
-		new SpringApplicationBuilder(Application.class).run(args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
 }
