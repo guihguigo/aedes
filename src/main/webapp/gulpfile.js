a569f1c5ccfb8c25e80076594c285664066b2181
GSuaki <gsuaki@hotmail.com>
2016-02-14 17:03:11 -0200

    Changing gulp file

--- src/main/webapp/gulpfile.js
+++ src/main/webapp/gulpfile.js
@@ -8,9 +8,10 @@ var options = {
 
 // Base build modules
 // ==========================
+// target: 'http://localhost:8080'
 options.modulesData = {
   proxy: {
-    target: 'http://localhost:8080'
+    target: 'http://ec2-52-11-89-206.us-west-2.compute.amazonaws.com:8080'
   }
 }
 
