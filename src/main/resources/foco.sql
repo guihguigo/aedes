3572c0cb945212750ff5baeaa1e12b028d64b6eb
GSuaki <gsuaki@hotmail.com>
2016-02-15 20:04:42 -0200

    Fotos do app

---
+++ src/main/resources/foco.sql
@@ -0,0 +1,23 @@
+
+
+INSERT INTO foco (codigo, nome, como_limpar) VALUES(3, 'Vasos de plantas', 'Use água, esponja e sabão para limpeza. Deposite areia na vasilha sob o vaso a cada limpeza.');
+
+INSERT INTO foco (codigo, nome, como_limpar) VALUES(4, 'Ralos', 'Limpe-os jogando água sanitária. Mantenha-os vedados caso não os utilize.');
+
+INSERT INTO foco (codigo, nome, como_limpar) VALUES(5, 'Recipientes com água', 'Use água, esponja e sabão para limpeza. (Jarras, garrafas, potes e baldes).');
+
+INSERT INTO foco (codigo, nome, como_limpar) VALUES(6, 'Caixa d’água', 'Use água, sabão e água sanitária para a limpeza. Mantenha a mesma tampada durante todo o tempo.');
+
+INSERT INTO foco (codigo, nome, como_limpar) VALUES(7, 'Bebedouros de Animais', 'Use água, esponja e sabão para limpeza. Mantenha-os limpos, trocando a água diariamente.');
+
+INSERT INTO foco (codigo, nome, como_limpar) VALUES(8, 'Piscinas', 'Use água, sabão e água sanitária para limpeza. O cloro deve estar sempre no nível adequado, tratando a água. Manter coberta, caso não esteja utilizando.');
+
+INSERT INTO foco (codigo, nome, como_limpar) VALUES(9, 'Calhas', 'Remova tudo que possa impedir a passagem da água.');
+
+INSERT INTO foco (codigo, nome, como_limpar) VALUES(10, 'Bromélias (Planta)', 'Dilua uma colher (sopa) de água sanitária em 1 litro de água limpa e regue-as. Eliminá-las poderia afetar o equilíbrio ecológico, portanto, o melhor é preservar de maneira correta.');
+
+INSERT INTO foco (codigo, nome, como_limpar) VALUES(11, 'Pneus Velhos', 'Mantenha-os em lugar coberto ou fure-os. Caso queira descartá-los, separe-os para serem levados pela coleta de lixo. Nunca os jogue em terrenos baldios.');
+
+INSERT INTO foco (codigo, nome, como_limpar) VALUES(12, 'Sacos de Lixo', 'Deve ser bem fechado e DIARIAMENTE depositado no lugar adequado para ser recolhidos pela coleta de lixo.');
+
+INSERT INTO foco (codigo, nome, como_limpar) VALUES(13, 'Geladeiras', 'A cafeína é um método natural, não prejudicial e fatal contra a dengue. Deposite na gaveta, abaixo, no exterior da geladeira, 4 colheres de sopa de borra de café, para cada 300 ml de água. ');
