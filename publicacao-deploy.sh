dad7565a06cb97479352d26167180afd3a971db8
GSuaki <gsuaki@hotmail.com>
2016-02-24 11:03:52 -0300

    Ajustes no dockerfile

--- publicacao-deploy.sh
+++ publicacao-deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 docker login -e $DOCKER_HUB_EMAIL -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD
 
+docker build -t guihguigo/aedes-web:$BUILD_TAG nginx
 docker build -t guihguigo/aedes-server:$BUILD_TAG tomcat
+
+docker push guihguigo/aedes-web:$BUILD_TAG
 docker push guihguigo/aedes-server:$BUILD_TAG
 
-docker build -t guihguigo/aedes-web:$BUILD_TAG nginx
-docker push guihguigo/aedes-web:$BUILD_TAG
 
