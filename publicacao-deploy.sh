285813ad8d85818ae4e54063c69332741aabbff7
GSuaki <gsuaki@hotmail.com>
2016-02-24 10:49:20 -0300

    Fix publicacao-deploy

--- publicacao-deploy.sh
+++ publicacao-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker login -e $DOCKER_HUB_EMAIL -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD
 
-docker build -t guihguigo/aedes-server:$BUILD_TAG java
+docker build -t guihguigo/aedes-server:$BUILD_TAG tomcat
 docker push guihguigo/aedes-server:$BUILD_TAG
 
 docker build -t guihguigo/aedes-web:$BUILD_TAG nginx
